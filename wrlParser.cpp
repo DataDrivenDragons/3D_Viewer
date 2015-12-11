e145d081bc9f50c32b4126ec4a98b82fde2b919f
Radmer van der Heyde <radmer.vanderheyde@students.olin.edu>
2015-12-11 11:02:47 -0500

    Parsing faces done

--- wrlParser.cpp
+++ wrlParser.cpp
@@ -34,18 +34,32 @@ public:
 	}
 };
 
-void addVertex (Vertices * Verts, regmatch_t * v, char * str, float * out, int * i){
+void addVertex (Vertices * Verts, regmatch_t * v, char * str, float * out, int i){
 	int length = v[0].rm_eo - v[0].rm_so;
 	char * newStr = (char *) malloc(sizeof(char)*(length+1));
 	strncpy(newStr, str+v[0].rm_so, length);
 	newStr[length] = '\0';
-	out[*i%3] = strtof(newStr, NULL);
-	Verts->add(out[*i%3]);
-	fprintf(stdout, "Found: %f\n", out[*i%3]);
-	*i+=1;
-	
-
+	out[i%3] = strtof(newStr, NULL);
+	Verts->add(out[i%3]);
+	// fprintf(stdout, "Found: %f\n", out[*i%3]);
 	free(newStr);
+}
+
+void pushFace (Vertices * Verts, regmatch_t * v, char * str, float * face, int i) {
+	int length = v[0].rm_eo - v[0].rm_so;
+	char * newStr = (char *) malloc(sizeof(char)*(length+1));
+	strncpy(newStr, str+v[0].rm_so, length);
+	newStr[length] = '\0';
+	int index = strtof(newStr, NULL);
+	fprintf(stdout, "%i\n", index);
+	index = index*3;
+	face[(i%3)*3] = Verts->array[index];
+	face[(i%3)*3+1] = Verts->array[index+1];
+	face[(i%3)*3+2] = Verts->array[index+2];
+	fprintf(stderr, "f1 %f %f %f\n", face[(i%3)*3], face[(i%3)*3+1], face[(i%3)*3+2]);
+	fprintf(stdout, "V %f %f %f\n", Verts->array[index], Verts->array[index+1], Verts->array[index+2]);
+	free(newStr);
+	i+=1;
 }
 
 
@@ -118,6 +132,7 @@ int parse(char * path) {
 	int length;
 	int i = 0;
 	float out[3];
+	float faceList[12];
 
 	ret = regcomp(&regF, "IndexedFaceSet", 0);
 	if (ret) {
@@ -137,7 +152,7 @@ int parse(char * path) {
 		exit(1);
 	}
 
-	ret = regcomp(&regInd, "-?[0-9]*", REG_EXTENDED);
+	ret = regcomp(&regInd, " [0-9]*", REG_EXTENDED);
 	if (ret) {
 		fprintf(stderr, "Could not compile regex for Indeces\n");
 		exit(1);
@@ -166,16 +181,6 @@ int parse(char * path) {
 			}
 		}
 
-		//Match faces in file, create face array from vertices, update shared
-		if (reading_faces) {
-			//( -?[0-9]*,){4}
-			if (first_faces) {
-				first_faces = false;
-			} else {
-
-			}			
-		}
-
 		//Match vertices in file, add vertices to Vertices. 
 		if (reading_verts && !done_verts) {
 			if (first_verts) {
@@ -183,8 +188,9 @@ int parse(char * path) {
 				do {
 					ret = regexec(&regCoor, vertInd+offset, 1, v, 0);
 					if (!ret) {
-						addVertex(&Verts, v, vertInd+offset, out, &i);
+						addVertex(&Verts, v, vertInd+offset, out, i);
 						offset += v[0].rm_eo;
+						i++;
 					}
 				} while (ret == 0);
 				tempStr = (char *) malloc(sizeof(char)*21);
@@ -203,41 +209,112 @@ int parse(char * path) {
 					do {
 						ret = regexec(&regCoor, temp2+offset, 1, v, 0);
 						if (!ret) {
-							addVertex(&Verts, v, temp2+offset, out, &i);
+							addVertex(&Verts, v, temp2+offset, out, i);
 							offset += v[0].rm_eo;
+							i++;
 						}
 					} while (ret == 0);
 					free(tempStr);
 					free(temp2);
 					done_verts = true;
-					continue;
 				} else {
 					strncpy(tempStr+10, buff, 10);
 					if (tempStr[0] != ' ') {
 						ret = regexec(&regCoor, tempStr, 1, v, 0);
 						if(!ret) {
-							addVertex(&Verts, v, tempStr, out, &i);
+							addVertex(&Verts, v, tempStr, out, i);
+							i++;
 						}
 					} else if (tempStr[10] != ' '){
 						ret = regexec(&regCoor, tempStr+9, 1, v, 0);
 						if(!ret) {
-							addVertex(&Verts, v, tempStr+9, out, &i);
+							addVertex(&Verts, v, tempStr+9, out, i);
+							i++;
 						}
 					}
 					offset = 0;
 					do {
 						ret = regexec(&regCoor, buff+offset, 1, v, 0);
 						if (!ret) {
-							addVertex(&Verts, v, buff+offset, out, &i);
+							addVertex(&Verts, v, buff+offset, out, i);
 							offset += v[0].rm_eo;
+							i++;
 						}
 					} while (ret == 0);
 					strncpy(tempStr, buff+BYTES_READ-10, 10);
 				}
 			}
 		}
+
+				//Match faces in file, create face array from vertices, update shared
+		if (reading_faces && !done_faces) {
+			//( -?[0-9]*,){4}
+			if (first_faces) {
+				offset = 0;
+				i = 0;
+				do {
+					ret = regexec(&regCoor, facInd+offset, 1, v, 0);
+					if (!ret) {
+						pushFace(&Verts, v, facInd+offset, faceList, i);
+						offset += v[0].rm_eo;
+						i++;
+					}
+				} while (ret == 0);
+				tempStr = (char *) malloc(sizeof(char)*11);
+				strncpy(tempStr, buff+BYTES_READ-5, 5);
+				tempStr[10] = '\0';
+				first_faces = false;
+			} else {
+				if (checkForEnd(buff)) {
+					char * tempInd;
+					tempInd = strstr(buff, "]");
+					length = tempInd-buff;
+					char * temp2 = (char *) malloc(sizeof(char)*length+1);
+					strncpy(temp2, buff, length);
+					temp2[length] = '\0';
+					offset = 0;
+					do {
+						ret = regexec(&regCoor, temp2+offset, 1, v, 0);
+						if (!ret) {
+							pushFace(&Verts, v, temp2+offset, faceList, i);
+							offset += v[0].rm_eo;
+							i++;
+						}
+					} while (ret == 0);
+					free(tempStr);
+					free(temp2);
+					done_faces = true;
+				} else {
+					strncpy(tempStr+10, buff, 10);
+					if (tempStr[0] != ' ') {
+						ret = regexec(&regCoor, tempStr, 1, v, 0);
+						if(!ret) {
+							pushFace(&Verts, v, tempStr, faceList, i);
+							i++;
+						}
+					} else if (tempStr[10] != ' '){
+						ret = regexec(&regCoor, tempStr+9, 1, v, 0);
+						if(!ret) {
+							pushFace(&Verts, v, tempStr+9, faceList, i);
+							i++;
+						}
+					}
+					offset = 0;
+					do {
+						ret = regexec(&regCoor, buff+offset, 1, v, 0);
+						if (!ret) {
+							pushFace(&Verts, v, buff+offset, faceList, i);
+							offset += v[0].rm_eo;
+							i++;
+						}
+					} while (ret == 0);
+					strncpy(tempStr, buff+BYTES_READ-5, 5);
+				}
+			}			
+		}
 	} while (res == BYTES_READ);
 	fprintf(stdout, "Verts length: %i\n", Verts.index);
+	fprintf(stdout, "%f %f %f\n", Verts.array[1059*3], Verts.array[1059*3+1],  Verts.array[1059*3+2]);
 	fprintf(stdout, "Last Vert: %f %f %f\n", Verts.array[Verts.index-3], Verts.array[Verts.index-2],Verts.array[Verts.index-1]);
 	fprintf(stdout, "Second to Last: %f %f %f\n", Verts.array[Verts.index-6], Verts.array[Verts.index-5], Verts.array[Verts.index-4]);
 	fclose(file);
